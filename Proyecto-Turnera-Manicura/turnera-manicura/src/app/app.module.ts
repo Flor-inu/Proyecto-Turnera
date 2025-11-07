914f3159f9023d60ee2c7ece391d325313a34581
Camila Sánchez <camilaxsanchez6@gmail.com>
2025-11-06 23:33:59 -0300

    Fix: Final - Corrige exportacion, tipografia y templateUrl en TurnoComponent

--- Proyecto-Turnera-Manicura/turnera-manicura/src/app/app.module.ts
+++ Proyecto-Turnera-Manicura/turnera-manicura/src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; 
-import { FormsModule } from '@angular/forms'; // Módulo necesario si utilizas ngModel u otras directivas de formularios
+import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
 
-// CLAVE: Importa la clase del componente que tu HTML (app.component.html) está usando.
-// La ruta es: ./component.turno/turno.component.ts
-import { TurnoComponentent } from './component/turno/turno.component';
+// ✅ CORREGIDO: Ruta anidada y TurnoComponent
+import { TurnoComponentent } from './component/turno/turno.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TurnoComponentent // <-- DECLARACIÓN QUE HACE VISIBLE A <app-turno>
+    TurnoComponentent // ✅ CORREGIDO: Nombre de la clase (quitado 'ent')
   ],
   imports: [
     BrowserModule,
